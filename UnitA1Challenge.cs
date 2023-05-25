49bc20ff3b30a33b226ddf3696df4e12572aa399
Algos132 <133154791+Algos132@users.noreply.github.com>
2023-05-25 17:47:56 -0600

    Debugged and Finished UnitA1Challenge.cs

--- UnitA1Challenge.cs
+++ UnitA1Challenge.cs
@@ -7,52 +7,47 @@ public class Programs
     public food vegitable;
     public food meat;
 	
-	public void Main()
-	{
+	public void Main(){
 		fruit = new food();
 		grain = new food();
         vegitable = new food();
         meat = new food();
+	{
 		
-		fruit.quantity = 3;
-        fruit.sustaining = 1.25f;}
-        public fruit.eatOne true;
+		fruit.quantity = 3;}
+        fruit.sustaining = 1.25f;
+		fruit.eatOne = true;
 
 		grain.quantity = 5;
-        grain.sustaining = 3f;}
+        grain.sustaining = 3f;
         grain.eatOne = false;
 
         vegitable.quantity = 4;
-        vegitable.sustaining = 2.5f;}
-        public vegitable.eatOne = false;
+        vegitable.sustaining = 2.5f;
+		vegitable.eatOne = false;
 
         meat.quantity = 2;
-		meat.sustaining = 4f};
-        meat.eatOne == true;
+		meat.sustaining = 4f;
 
 		
-		Console.WriteLine("you have " + fruit.quantity);
+		Console.WriteLine("you have " + fruit.quantity + "fruit.");
         if (fruit.eatOne == true)
-            Console.WriteLine("You've eaten one + " + fruit.sustaining + " sustenance.");
-        }
+            Console.WriteLine("You've eaten one fruit gained + " + fruit.sustaining + " sustenance.");
 
-		Console.WriteLine(grain.quantity);
+		Console.WriteLine ("you have " + grain.quantity + " grain.");
         if (grain.eatOne == true)
-            Console.WriteLine("You've eaten one grain +" + grain.sustaining + " sustenance.");
+            Console.WriteLine ("You've eaten one grain +" + grain.sustaining + " sustenance.");
 
-        Console.WriteLine(vegitable.quantity);
+        Console.WriteLine("you have " + vegitable.quantity + " vegitable.");
         if (grain.eatOne == true)
-            Console.WriteLine("You've eaten one grain +" + vegitable.sustaining + " sustenance.");
+            Console.WriteLine ("You've eaten one vegitable +" + vegitable.sustaining + " sustenance.");
 
-        Console.WriteLine(meat.quantity);
+        Console.WriteLine("you have " + meat.quantity + " meat.");
         if (meat.eatOne == true)
-            Console.WriteLine("You've eaten one mean +" + meat.sustaining + " sustenance.");
-
-
+            Console.WriteLine ("You've eaten one meat +" + meat.sustaining + " sustenance.");
 	}
-
 public class food {
 	public int quantity;
 	public float sustaining;
-	public bool eatOne;
+	public bool eatOne = true;}
 }
