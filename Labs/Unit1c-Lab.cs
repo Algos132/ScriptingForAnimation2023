2533587fba317a399c9df0465b62fd773e27f7f6
Algos132 <133154791+Algos132@users.noreply.github.com>
2023-06-06 18:44:53 -0600

    Succefully created a number pyramid in Unit1c-Lab.cs

---
+++ Labs/Unit1c-Lab.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Program
+{
+    public static void Main() {
+	
+		for(int i = 0; i <= 9; i++) {
+			for (int j = 0; j < i; j++) {
+				Console.Write(i);
+			}
+			Console.WriteLine();
+			
+		}
+		
+	}
+}
