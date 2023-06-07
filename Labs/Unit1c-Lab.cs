8b371463aab5a92d50d7afe90d576e599e6e53b7
Algos132 <133154791+Algos132@users.noreply.github.com>
2023-06-06 18:48:22 -0600

    Added comments to Unit1c-Lab.cs

--- Labs/Unit1c-Lab.cs
+++ Labs/Unit1c-Lab.cs
@@ -3,11 +3,16 @@ using System;
 public class Program
 {
     public static void Main() {
-	
+
+	//changing the number after "i <= " will expand or shrink the number pyramid.
 		for(int i = 0; i <= 9; i++) {
+
+            //writes the value of i while until j = i
 			for (int j = 0; j < i; j++) {
 				Console.Write(i);
 			}
+            
+            //creates a new line to separate the pyramid's "layers"
 			Console.WriteLine();
 			
 		}
