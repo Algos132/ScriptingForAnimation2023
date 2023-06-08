d5bd252df88fb33c5a9b5e93f38eb4c67d90d189
Algos132 <133154791+Algos132@users.noreply.github.com>
2023-06-08 16:04:24 -0600

    Added comments to Unit1c-Lab.cs

--- Labs/Unit1c-Lab.cs
+++ Labs/Unit1c-Lab.cs
@@ -4,8 +4,12 @@ public class Program
 {
     public static void Main() {
 
-	//changing the number after "i <= " will expand or shrink the number pyramid.
-		for(int i = 0; i <= 9; i++) {
+		//This asks the user to input a number that will determine how long the number pyramid will be.
+		Console.WriteLine("Enter a number:");
+		int x = Convert.ToInt32(Console.ReadLine());
+		
+	//changing the number after "i <= x" will expand or shrink the number pyramid.
+		for(int i = 0; i <= x; i++) {
 
             //writes the value of i while until j = i
 			for (int j = 0; j < i; j++) {
