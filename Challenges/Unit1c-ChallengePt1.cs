f87863d4e66d193d5186e8ccf95af69a3c0b2f1b
Algos132 <133154791+Algos132@users.noreply.github.com>
2023-06-08 19:39:18 -0600

    Added comments to code.

--- Challenges/Unit1c-ChallengePt1.cs
+++ Challenges/Unit1c-ChallengePt1.cs
@@ -4,11 +4,13 @@ public class Program
 {
 	public static void Main()
 	{
+        //sets the number that the player needs to guess.
 		Random rnd = new Random();
 		int guessThis = rnd.Next(1, 10);
 		int number = 0;
-
+        //sets the attempt to "0" which will be added to every attempt the player makes.
 		int x = 0;
+        //This sets the number of attempts you can make.
 			if(x != 3){
 				while (guessThis != number)
 				{
@@ -27,6 +29,8 @@ public class Program
 						Console.WriteLine("Good job! The correct answer was " + guessThis + "!");
 						break;
 					}
+                    //Since x is the number of the attempt you're on (-1) when you finish your third attempt,
+                    //this will display this message and end the game.
 					if(x == 3){
 						Console.WriteLine("Sorry, you're out of attempts. The correct answer was " + guessThis + ".");
 						break;
