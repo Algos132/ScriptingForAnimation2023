9a5e0ad1ee3bbd20aed7423f27b4b548d92d51a1
Algos132 <133154791+Algos132@users.noreply.github.com>
2023-06-15 14:55:07 -0600

    Updated code to correct version

--- UnityScripts/PlayerMovement.cs
+++ UnityScripts/PlayerMovement.cs
@@ -17,7 +17,7 @@ public class PlayerMovement : MonoBehaviour
 	// Start is called before the first frame update
 	void Start()
 	{
-		controller = GetComponent"Capsule"();
+		controller = GetComponent<CharacterController>();
 	}
 
     // Update is called once per frame
