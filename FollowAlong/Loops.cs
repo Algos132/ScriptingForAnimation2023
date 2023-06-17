962325a88ed80411a553b07b271905471baa1c1e
Algos132 <133154791+Algos132@users.noreply.github.com>
2023-06-16 22:51:43 -0600

    Created Loops.cs

---
+++ FollowAlong/Loops.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using System.Collections;
+
+//-------
+//For Loops
+public class ForLoop : MonoBehaviour
+{
+    int numEnemies = 3;
+
+
+    void Start()
+    {
+        for(int i = 0; i < numEnemies; i++)
+        {
+            Debug.Log("Creating enemy number: " + i);
+        }
+
+    }
+}
+//-------
+//WhileLoops
+public class WhileLoop : MonoBehaviour
+{
+    int cupsInTheSink = 4;
+
+
+    void Start()
+    {
+        while(cupsInTheSink > 0)
+        {
+            Debug.Log ("I've washed a cup!");
+            cupsInTheSink--;
+        }
+    }
+}
+//-------
+//DoWhileLoops
+public class DoWhileLoop : MonoBehaviour
+{
+    void Start()
+    {
+        bool shouldContinue = false;
+
+        do
+        {
+            print("Hello World");
+
+        }while(shouldContinue == true)
+    }
+}
+//-------
+//ForeachLoop
+public class ForeachLoop : MonoBehaviour
+{
+    void Start()
+    {
+        string[] strings = new string[3];
+
+        strings[0] = "First string";
+        strings[1] = "Second string";
+        strings[2] = "Third string";
+
+        foreach(string item in strings)
+        {
+            print(item);
+        }
+    }
+}
