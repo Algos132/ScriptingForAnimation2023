569cbac4c2729a865a3e61f135c3ced9cd6debef
Algos132 <133154791+Algos132@users.noreply.github.com>
2023-06-17 18:02:18 -0600

    Created Destroy.cs

---
+++ FollowAlong/Destroy.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System.Collections;
+
+public class DestroyBasic : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
+public class DestroyOther : MonoBehaviour
+{
+    public GameObject other;
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            Destroy(other);
+        }
+    }
+}
+
+public class DestroyComponent
+{
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            Destroy(GetComponent<MeshRenderer>());
+        }
+    }
+}
--- UnityScripts/PlayerMovement.cs
+++ UnityScripts/PlayerMovement.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 	[SerializeField]public float moveSpeed = 5f;
-	[SerializeField]private float jumpForce = 5f;
-	[SerializeField]private float gravity = 9f;
+	[SerializeField]public float jumpForce = 5f;
+	[SerializeField]private float gravity = -9.81f;
 
 
-	private CharacterController controller;
+	CharacterController controller;
 	private Vector3 moveDirection;
 	private bool isJumping;
+	private float ySpeed;
 	
 
 	// Start is called before the first frame update
@@ -23,18 +24,17 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-	    Move();
-    }
-	private void Move()
-	{
-		float moveHorizontal = Input.GetAxis("Horizontal");
+	    float moveHorizontal = Input.GetAxis("Horizontal");
 		float moveVertical = Input.GetAxis("Vertical");
-
-		if (controller.isGrounded) jumpForce = 0;
-		else jumpForce -= gravity * Time.deltaTime;
-
+		
 		Vector3 gravityMove = new Vector3(0, jumpForce, 0);
 		Vector3 move = transform.forward * moveVertical + transform.right * moveHorizontal;
 		controller.Move(moveSpeed * Time.deltaTime * move);
-	}	
+		ySpeed += Physics.gravity.y * Time.deltaTime;
+		
+		if(Input.GetButtonDown("jump"))
+		{
+			ySpeed = jumpForce;
+		}
+    }	
 }
