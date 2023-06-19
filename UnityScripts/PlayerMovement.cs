6806996f7e049e8cef614d143c86f9dc15f18aad
Algos132 <133154791+Algos132@users.noreply.github.com>
2023-06-19 11:49:28 -0600

    Updated PlayerMovement.cs

--- UnityScripts/PlayerMovement.cs
+++ UnityScripts/PlayerMovement.cs
@@ -1,40 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
-	[SerializeField]public float moveSpeed = 5f;
-	[SerializeField]public float jumpForce = 5f;
-	[SerializeField]private float gravity = -9.81f;
+    [SerializeField]public float moveSpeed = 5f;
+    [SerializeField]public float jumpForce = 5f;
+    [SerializeField]private float gravity = 9.81f;
 
 
-	CharacterController controller;
-	private Vector3 moveDirection;
-	private bool isJumping;
-	private float ySpeed;
+    CharacterController controller;
+    private bool isJumping;
+    private float ySpeed;
+    private float moveDirection;
 	
 
-	// Start is called before the first frame update
-	void Start()
-	{
-		controller = GetComponent<CharacterController>();
-	}
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
 
     // Update is called once per frame
     void Update()
     {
-	    float moveHorizontal = Input.GetAxis("Horizontal");
-		float moveVertical = Input.GetAxis("Vertical");
-		
-		Vector3 gravityMove = new Vector3(0, jumpForce, 0);
-		Vector3 move = transform.forward * moveVertical + transform.right * moveHorizontal;
-		controller.Move(moveSpeed * Time.deltaTime * move);
-		ySpeed += Physics.gravity.y * Time.deltaTime;
-		
-		if(Input.GetButtonDown("jump"))
-		{
-			ySpeed = jumpForce;
-		}
-    }	
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        
+        Vector3 move = new Vector3(moveHorizontal, 0, moveVertical);
+
+        move.y = moveDirection;
+        
+
+        
+        if (controller.isGrounded)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDirection = jumpForce;
+            }
+        }
+        else
+        {
+            moveDirection += gravity * Time.deltaTime;
+        }
+        
+        controller.Move(move * moveSpeed * Time.deltaTime);
+
+    }
 }
