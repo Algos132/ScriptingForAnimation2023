916f42242c862d3ccd3f05a75e1a4755f20313a8
Mike Heil <mikeph132@gmail.com>
2023-06-21 18:53:46 -0600

    Created UFO game and 3 scripts

---
+++ UFO Defense Force/Assets/scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float xRange;
+    public float speed;
+
+    public Transform blaster;
+    public GameObject laserBolt;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Initializes horizontalInput to recieve values from keyboard.
+        horizontalInput = Input.GetAxis("Horizontal");
+        
+        //Moves Player horizontally (left and right).
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        //keeps player inbounds. (left)
+        if (transform.position.x < -xRange) 
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        //keeps player inbounds. (right)
+        if (transform.position.x > xRange) 
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Create laserBolt at the blaster transform position. Maintaining the rotation.
+            Instantiate(laserBolt, blaster.transform.position, laserBolt.transform.rotation);
+        }
+
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+    
+}
